177d65dcee71b634e69f7a35ded310c6ad67963d
Jakob Fender <jakob.fender@hslu.ch>
2024-05-10 18:22:38 +0200

    remove links

--- Home.py
+++ Home.py
@@ -6,17 +6,11 @@ st.title('Color Quantization and Dithering')
 """ This app was created for the Computer Vision & AI course at the University of Applied Sciences Lucerne.
 The app allows you to perform color quantization and dithering on an image of your choice."""
 
+""" To get started, select a page on the left sidebar."""
+
 """ The code can be found on [GitHub](https://github.com/JakobFenderHSLU/auto-pixler)"""
 
 st.write("---")
-
-st.write("### Color Quantization")
-st.write("- [Color Quantization](/Color_Quantisation)")
-st.write("- [Color Quantization with a Specific Palette](/Color_Quantisation_Specific_Palette)")
-
-st.write("### Dithering")
-st.write("- [Dithering](/Dithering)")
-st.write("- [Dithering with a Specific Palette](/Dithering_Specific_Palette)")
 
 
 
