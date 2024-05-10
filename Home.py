28767ee774b9b165f91accab9419aba49d46f167
Jakob Fender <jakob.fender@hslu.ch>
2024-05-10 18:20:56 +0200

    fix link

--- Home.py
+++ Home.py
@@ -11,12 +11,12 @@ The app allows you to perform color quantization and dithering on an image of yo
 st.write("---")
 
 st.write("### Color Quantization")
-st.write("- [Color Quantization](/1_Color_Quantisation)")
-st.write("- [Color Quantization with a Specific Palette](/2_Color_Quantisation_Specific_Palette)")
+st.write("- [Color Quantization](/Color_Quantisation)")
+st.write("- [Color Quantization with a Specific Palette](/Color_Quantisation_Specific_Palette)")
 
 st.write("### Dithering")
-st.write("- [Dithering](/3_Dithering)")
-st.write("- [Dithering with a Specific Palette](/4_Dithering_Specific_Palette)")
+st.write("- [Dithering](/Dithering)")
+st.write("- [Dithering with a Specific Palette](/Dithering_Specific_Palette)")
 
 
 
